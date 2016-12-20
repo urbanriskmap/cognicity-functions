e25b0382357c79ebde43602a2c7aefa4646c9e00
mgvarley <b03d66df56994e4e0f2534fa616cdb06b97896e0>
2016-12-20 17:07:58 +0100

    Added check for existing image and call to patch the card report with the image url

--- cards/index.js
+++ cards/index.js
@@ -3,76 +3,129 @@
 console.log('Loading function');
 
 const https = require('https');
+const querystring = require('querystring');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3( { params: { Bucket: process.env.CARD_IMAGE_BUCKET } } );
 
 // Check if the card exists
-const checkCardExists = (cardId) => new Promise((resolve, reject) => {
-    // Make a call to the URL to ensure that the card exists
-    let url = [process.env.COGNICITY_URL,'cards',cardId].join('/');
-    console.log(url);
-    https.get(url, (res) => {
-        // If status is 200 then exists, otherwise it does not
-        resolve(res.statusCode === 200);
-    }).on('error', (err) => {
-        console.log('Error, with: ' + err.message);
-        return reject(err);
+const retrieveCard = (cardId) => new Promise((resolve, reject) => {
+  // Make a call to the URL to ensure that the card exists
+  let url = [process.env.COGNICITY_URL,'cards',cardId].join('/');
+  console.log(url);
+  https.get(url, (res) => {
+    // If status is 404 then no card so return null
+    if (res.statusCode === 404) {
+      resolve(null);
+      return;
+    }
+    // If status is not 200 then we have a problem so return error
+    if (res.statusCode !== 200) {
+      reject(new Error('Problem retrieving card'));
+      return;
+    }
+    // Try and parse the card record
+    let body = ''
+    res.on('data', (data) => {
+      body += data;
     });
+    res.on('end', () => {
+      try {
+        // Return the parsed card
+        resolve(JSON.parse(body));
+      } catch(e) {
+        // OR reject with an error if the JSON could not be parsed
+        console.log('malformed request', body);
+        reject(new Error('malformed request: ' + body));
+        return;
+      }
+    });
+  }).on('error', (err) => {
+    // Something bad happened so error
+    console.log('Error, with: ' + err.message);
+    reject(err);
+    return;
+  });
 });
 
 // Upload the image to S3
 const uploadImage = (cardId, contentType, base64Image) => {
-    // Setup the S3 payload
-    const params = {
-        Key: [cardId, (contentType ? contentType.split('/')[1] : 'png')].join('.'),
-        Body: new Buffer(base64Image,'base64'),
-        ContentEncoding: 'base64',
-        ContentType: contentType || 'image/png'
-    };
-    return s3.putObject(params).promise();
+  // Setup the S3 payload
+  const params = {
+    Key: [cardId, (contentType ? contentType.split('/')[1] : 'png')].join('.'),
+    Body: new Buffer(base64Image,'base64'),
+    ContentEncoding: 'base64',
+    ContentType: contentType || 'image/png'
+  };
+  return s3.putObject(params).promise();
 }
 
-// TODO: Update card record with details of the image
-const updateCardImage = (cardId) => new Promise((resolve, reject) => {
-    resolve(true);
+// Update card record with details of the image
+const updateCardImage = (cardId, contentType) => new Promise((resolve, reject) => {
+  let patch = querystring.stringify({
+    image_url: [cardId, (contentType ? contentType.split('/')[1] : 'png')].join('.')
+  });
+  let req = https.request({
+    hostname: COGNICITY_URL.replace('https://',''),
+    port: 443,
+    path: `/cards/${cardId}`,
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(post_data)
+    }
+  }, (res) => {
+    // If image was successfully patched return true else return false
+    resolve(res.statusCode === 200);
+    return;
+  });
+  req.on('error', (e) => {
+    reject(err);
+    return;
+  });
+  req.write(patch);
+  req.end();
 });
 
 // Handle upload of an image for a given card, if exists add to S3 and update card record
 exports.handler = (event, context, callback) => {
 
-    // Helper function to format the response
-    const done = (err, statusCode, res) => callback(err ? JSON.stringify({
-        statusCode: statusCode,
-        message: err.message
-    }): null,
-    {
-        statusCode: statusCode,
-        result: res
-    });
+  // Helper function to format the response
+  const done = (err, statusCode, res) => callback(err ? JSON.stringify({
+    statusCode: statusCode,
+    message: err.message
+  }): null,
+  {
+    statusCode: statusCode,
+    result: res
+  });
 
-    // Check the required parameters
-    if (!event.cardId) return done({ message: 'cardId is required' }, 400);
-    if (!event.base64Image) return done({ message: 'base64Image is required' }, 400);
+  // Check the required parameters
+  if (!event.cardId) return done({ message: 'cardId is required' }, 400);
+  if (!event.base64Image) return done({ message: 'base64Image is required' }, 400);
 
-    // We have the parameters we need, lets proceed
-    try {
-        // Check the card exists
-        checkCardExists(event.cardId).then((exists) => {
-            if (!exists) return done({message: `No card exists with cardId '${event.cardId}'`}, 404);
+  // We have the parameters we need, lets proceed
+  try {
+    // Check the card exists
+    retrieveCard(event.cardId).then((card) => {
+      if (!card) return done({message: `No card exists with cardId '${event.cardId}'`}, 404);
+      if (card.report && card.report.image_url) return done({message: `This card already has an image '${event.cardId}'`}, 409);
 
-            // Card exists so let's try and upload the image to S3
-            uploadImage(event.cardId, event.contentType, event.base64Image).then((res) => {
-                console.log('Image upload successfully');
+      // Card exists and does not have an image so let's try and upload the image to S3
+      uploadImage(event.cardId, event.contentType, event.base64Image).then((res) => {
+        console.log('Image upload successfully');
 
-                // Finally, update the card with the image details
-                updateCardImage(event.cardId, event.contentType).then((res) => {
-                    console.log('Card updated with image');
-                    return done(null, 200, { cardId: event.cardId, updated: true });
-                });
-            });
-        })
-    } catch (err) {
-        // console.log('An error occured, ' + err);
-        return done(err, 500);
-    }
+        // Finally, update the card with the image details
+        updateCardImage(event.cardId, event.contentType).then((updated) => {
+          // Return a success
+          if (updated) return done(null, 200, { cardId: event.cardId, updated: true });
+          // Return an error giving details, we have an inconsistent state
+          // TODO: Perhaps in the future we should back out the image if this happens?
+          else return done({message: `An image was uploaded but the card record could not be updated for cardId '${event.cardId}'`}, 409);
+        });
+      });
+    })
+  } catch (err) {
+    // console.log('An error occured, ' + err);
+    return done(err, 500);
+  }
 };
